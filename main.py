a4efd2ccbae4f301ab9028ad89c4f22f3bfb0dab
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-01-14 00:51:02 +0200

    Refactor main, add loop that deletes listings that were not found on a pass

--- main.py
+++ main.py
@@ -8,25 +8,38 @@ from prisma.models import Listing
 
 
 driver = webdriver.Chrome()
+
+# GET MAX PAGE FROM PAGINATION
 last_page = int(utils.get_max_page(driver))
 
 
 async def main() -> None:
+
     db = Prisma(auto_register=True)
     await db.connect()
 
+    listing_urls = await Listing.prisma().find_many()
+
+    # LIST OF URLS, USED TO DELETE LISTINGS FROM DB THAT WAS MISSED ON A LOOP
+    active_listing_dict = utils.get_url_dict(listing_urls)
+
+    # LOOP OIKOTIE LISTING PAGES
     for i in range(1, last_page + 1):
-        print(i)
+        print("page: " + str(i))
         URL = utils.get_url(i)
         driver.get(URL)
-        time.sleep(0.5)
+        time.sleep(1.5)
         soup = BeautifulSoup(driver.page_source, "html.parser")
         elems = soup.find_all("div", "cards-v2__card")
         for item in elems:
-
+            # SCRAPE RELEVANT DATA
             sale_listing_object = utils.get_data_from_card(item)
 
             if sale_listing_object != None:
+                listing_url = sale_listing_object["url"]
+                # FLAG URL FOUND TO NOT BE DELETED
+                if listing_url in active_listing_dict:
+                    active_listing_dict[listing_url] = True
                 try:
                     await Listing.prisma().create(
                         data=sale_listing_object
@@ -34,8 +47,21 @@ async def main() -> None:
                 except Exception:
                     pass
 
-    driver.quit()
+    total_row_dels = 0
+
+    # DEL LISTINGS IN DB THAT WASNT FOUND ON A PASS
+    for (key, value) in active_listing_dict.items():
+        if value == False:
+            await Listing.prisma().delete(where={
+                "url": value
+            })
+            total_row_dels += 1
+
+    print("-----------------------------------")
+    print("total row dels: " + str(total_row_dels))
+    print("-----------------------------------")
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
+if __name__ == "__main__":
+    while True:
+        asyncio.run(main())
